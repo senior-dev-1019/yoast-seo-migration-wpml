6cb79705b53acd4f8b0dcac199e8aacaf50301d5
senior-developer1019 <romo1112@yandex.com>
2021-09-03 20:39:49 +0900

    script for all in one to yoast seo

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,8 @@
     jQuery(function(){
         jQuery('#ysw_migration_start').click(ysw_migration_batch);
+        jQuery('#ysa_migration_start').click(ysa_migration_batch);
         
         function ysw_migration_batch(){
-            
             jQuery('#ysw_migration_working').fadeIn();
             jQuery('#ysw_migration_start').attr('disabled', 'disabled');
             jQuery.ajax({
@@ -20,5 +20,23 @@
                                     
             })                        
         }
-        
+                
+        function ysa_migration_batch(){
+            jQuery('#ysa_migration_working').fadeIn();
+            jQuery('#ysa_migration_start').attr('disabled', 'disabled');
+            jQuery.ajax({
+                type: "POST",
+                url: ajaxurl,
+                dataType: 'json',
+                data: 'action=ysa_migration_ajx',
+                success: function(res){
+                    jQuery('#ysa_migration_status:hidden').show();
+                    jQuery('#ysa_migration_status').html(res.messages.join('<br />') + jQuery('#ysa_migration_status').html());
+                    alert(res.messages);
+                    jQuery('#ysa_migration_working').fadeOut();
+                    jQuery('#ysa_migration_start').removeAttr('disabled');
+                }
+                                    
+            })                        
+        }
     })
--- yoast-seo-migration-wpml.php
+++ yoast-seo-migration-wpml.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Plugin Name: Migration Yoast Seo to WPML
+Plugin Name: Yoast Seo to WPML Migration 
 Description: A plugin for migration yoast Seo metadatas when migration for wp multig
 Plugin URI: https://elementor.com/?utm_source=wp-plugins&utm_campaign=plugin-uri&utm_medium=wp-dash
 Author: Senior Dev
@@ -23,8 +23,6 @@ class YSW_migrationer {
 	    global $wpdb;
 	    $this->wpdb = $wpdb;
 		$this->add_hooks();
-        // $this->set_qt_default_language();
-        // $this->set_qt_active_languages();
 
 	}
 
@@ -32,6 +30,7 @@ class YSW_migrationer {
 		add_action( 'init', array( $this, 'init' ), 101 );
 		add_action( 'admin_menu', array( $this, 'yoast_seo_migration_plugin_setup_menu' ) );
 		add_action( 'wp_ajax_ysw_migration_ajx', array( $this, '_ysw_migration' ) );
+		add_action( 'wp_ajax_ysa_migration_ajx', array( $this, '_ysa_migration' ) );
     }
 
 	public function init() {
@@ -55,7 +54,7 @@ class YSW_migrationer {
 	        <p>
 	            <input type="button" id="ysw_migration_start"
 	                   value="<?php esc_attr_e( 'Start', 'ysw_migration' ) ?>"
-	                   class="button-primary" />
+	                   class="button-primary" disabled />
 	            &nbsp;<span id="ysw_migration_working"
 	                        style="display:none;"><?php _e( 'Working... <br> Remember this is early development version. If you notice any problem <a href="https://wpml.org/forums/" target="_blank">please report in WPML support forum</a>', 'ysw_migration' ) ?></span>
 	        </p>
@@ -64,7 +63,96 @@ class YSW_migrationer {
 	        <div id="ysw_migration_status"
 	             style="max-height:360px;overflow: auto;font-size:10px;background-color: #eee;padding:5px;border:1px solid #ddd;margin-bottom:8px;display:none;"></div>
 	     </div>
+
+	    <div class="wrap">
+	        <h2>
+		<?php echo __( 'Migration to Yoast Seo from All in One Seo ', 'ysa_migration' ) ?></h2>
+
+	        <p>
+	            <input type="button" id="ysa_migration_start"
+	                   value="<?php esc_attr_e( 'Start', 'ysa_migration' ) ?>"
+	                   class="button-primary" />
+	            &nbsp;<span id="ysa_migration_working"
+	                        style="display:none;"><?php _e( 'Working... <br> Remember this is early development version. If you notice any problem <a href="https://wpml.org/forums/" target="_blank">please report in WPML support forum</a>', 'ysa_migration' ) ?></span>
+	        </p>
+
+
+	        <div id="ysa_migration_status"
+	             style="max-height:360px;overflow: auto;font-size:10px;background-color: #eee;padding:5px;border:1px solid #ddd;margin-bottom:8px;display:none;"></div>
+	     </div>
 	     <?php
+	}
+
+	public function _ysa_migration(){
+		$origin_seo_rows = $this->get_origin_seo_rows_to_migration();
+		if ( $origin_seo_rows ) {
+			$origin_seo_results = $this->process_origin_seo_rows_batch( $origin_seo_rows );
+			$this->response['messages'][0] = "Migration Successed !";
+		} else {
+			$this->response['messages'][0] = __( 'No All in One Seo rows to migrate.', 'ysa-migrate' );
+		}
+		$aio_seo_rows = $this->get_postmeta_aio_rows_to_migration();
+		if( $aio_seo_rows ) {
+			$aio_seo_results = $this->process_aio_seo_rows_batch( $aio_seo_rows );
+			$this->response['messages'][0] = "Migration Successed !";
+		}
+
+		echo json_encode( $this->response );
+		exit;
+	}
+
+	public function get_origin_seo_rows_to_migration(){
+		return $this->wpdb->get_results( "
+            SELECT * FROM `".$this->wpdb->base_prefix."aioseo_posts`
+            WHERE 	title IS NOT NULL
+			" );
+	}
+
+	private function process_origin_seo_rows_batch( $rows ) {
+		foreach ($rows as $key => $row) {
+			$yoast_row = $this->wpdb->get_results("
+				SELECT * FROM ".$this->wpdb->base_prefix."yoast_indexable
+				WHERE object_id = '".$row->post_id."'");
+			if(!empty($yoast_row)){
+				$vowels = array("| #site_title");
+				$row->title = str_replace($vowels, "%%sep%% %%sitename%%", $row->title);
+				$id = $this->wpdb->update($this->wpdb->base_prefix."yoast_indexable", array('title' => $row->title, 'description' => $row->description), array('object_id'=> $row->post_id));
+				return $id;
+			}
+		}
+	}
+
+	public function get_postmeta_aio_rows_to_migration(){
+		return $this->wpdb->get_results( "
+            SELECT * FROM ".$this->wpdb->base_prefix."postmeta
+            WHERE 	meta_key = '_aioseop_title' OR 
+					meta_key = '_aioseop_description'
+			" );
+	}
+
+	private function process_aio_seo_rows_batch( $rows ) {
+		foreach ( $rows as $key => $row ) {
+			$rows[$key] = $this->process_aio_postmeta_row( $row );
+		}
+		return $rows;
+	}
+
+	private function process_aio_postmeta_row( $row ) {
+		if($row->meta_key === "_aioseo_title"){
+			$vowels = array("| #site_title");
+			$row->meta_value = str_replace($vowels, "%%sep%% %%sitename%%", $row->meta_value);
+			$result = $this->wpdb->update($this->wpdb->base_prefix."postmeta", array("meta_value" => $row->meta_value), array('post_id' => $row->post_id, "meta_key" => "_yoast_wpseo_title"));
+			if ($result === FALSE || $result < 1) {
+			    $this->wpdb->insert($this->wpdb->base_prefix."postmeta", array("post_id" => $row->post_id, "meta_key" => "_yoast_wpseo_title", "meta_value" => $row->meta_value));
+			}
+		}
+		if($row->meta_key === "_aioseo_description"){
+			$result = $this->wpdb->update($this->wpdb->base_prefix."postmeta", array("meta_value" => $row->meta_value), array('post_id' => $row->post_id, "meta_key" => "_yoast_wpseo_metadesc"));
+			if ($result === FALSE || $result < 1) {
+			    $this->wpdb->insert($this->wpdb->base_prefix."postmeta", array("post_id" => $row->post_id, "meta_key" => "_yoast_wpseo_metadesc", "meta_value" => $row->meta_value));
+			}
+		}
+		return true;
 	}
 
 	public function _ysw_migration(){
@@ -72,22 +160,22 @@ class YSW_migrationer {
 		if ( $yoast_indexable_rows ) {
 			$yoast_indexable_results = $this->process_yoast_indexable_rows_batch( $yoast_indexable_rows );
 		} else {
-			$this->response['messages'] = __( 'No yoast_indexable rows to migrate.', 'ysw-migrate' );
+			$this->response['messages'][0] = __( 'No yoast_indexable rows to migrate.', 'ysw-migrate' );
 		}
 		$postmeta_rows = $this->get_postmeta_rows_to_miration( self::BATCH_SIZE );
 		if ( $postmeta_rows ) {
 			$postmeta_results = $this->process_postmeta_rows_batch( $postmeta_rows );
 		} else {
-			$this->response['messages'] = __( 'No postmeta rows to migrate.', 'postmeta-migrate' );
+			$this->response['messages'][0] = __( 'No postmeta rows to migrate.', 'postmeta-migrate' );
 		}
 		$post_rows = $this->get_post_rows_to_miration( self::BATCH_SIZE );
 		if ( $post_rows ) {
 			$post_results = $this->process_post_rows_batch( $post_rows );
 		} else {
-			$this->response['messages'] = __( 'No post rows to migrate.', 'post-migrate' );
+			$this->response['messages'][0] = __( 'No post rows to migrate.', 'post-migrate' );
 		}
 		if( count($yoast_indexable_rows) === count($yoast_indexable_results) && count($postmeta_rows) === count($postmeta_results) && count($post_rows) === count($post_results) )
-			$this->response['messages'] = "Migration Successed";
+			$this->response['messages'][0] = "Migration Successed !";
 
 		echo json_encode( $this->response );
 		exit;
